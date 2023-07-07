499e3235b4604419c6f39a8f0fab34f2a0e6ae97
Franco Altuna <franco.altuna@me.com>
2023-07-07 00:58:11 -0300

    Working

--- predict.js
+++ predict.js
@@ -38,64 +38,15 @@ async function analyzeTimeSeries() {
     for (let i = 0; i < data.length; i++) {
         inputData.push(data[i]);
     }
-    console.log("Input dataset lenth", inputData.length);
-    const inputShape = [inputData.length, 13];
-    console.log("Input shape", inputShape);
-    const batchSize = 128;
-    const epochs = 20;
-    console.log("Parameters", {
-        batchSize,
-        epochs
-    });
-    console.log("Preparing input tensor");
-    const inputTensor = tf.tensor(inputData, inputShape);
-    console.log("Input tensor", { ...inputTensor });
     const model = await loadModel();
-    const trainingInputData = [];
-    const trainingTargetData = [];
-    for (let i = 0; i < inputData.length; i++) {
-        trainingInputData.push(inputData[i]);
-        trainingTargetData.push((inputData[i + 1] || inputData[i])[0]);
-    }
-    const trainingInputTensor = tf.tensor(trainingInputData, [trainingInputData.length, 1, trainingInputData[0].length]);
-    console.log({
-        trainingInputTensor
-    });
-    const trainingOutputTensor = tf.tensor1d(trainingTargetData);
-    console.log({
-        trainingOutputTensor
-    });
-    model.compile({
-        optimizer: "adam",
-        loss: "meanSquaredError"
-    });
-    let loss;
-    await model.fit(trainingInputTensor, trainingOutputTensor, {
-        batchSize,
-        epochs,
-        shuffle: false,
-        validationSplit: 0.2,
-        callbacks: {
-            onEpochEnd: (epoch, logs) => {
-                loss = logs.loss;
-            }
-        }
-    });
-    await saveModel(model);
-    console.log("Training completed, loss:", loss);
-
-    // Prepare the input data for prediction
-    const predictionInputData = inputData.slice(-1); // Use the last 10 data points for prediction
+    const predictionInputData = inputData.slice(-10);
     const predictionInputTensor = tf.tensor(predictionInputData, [predictionInputData.length, 1, predictionInputData[0].length]);
-    console.log({
-        predictionInputTensor
-    });
-    // Make predictions
     const predictions = model.predict(predictionInputTensor);
-    const predictedValues = Array.from(predictions.dataSync()); // Convert tensor to an array
-
-    // Display the predicted values
-    console.log("Predicted values:", predictedValues.map(denormalize())[0], inputData.slice(-1)[0].map(denormalize())[0]);
+    const predictedValues = Array.from(predictions.dataSync());
+    console.log("Prediction", {
+        original: Utils.round(inputData.slice(-1)[0].map(denormalize())[0]),
+        prediction: Utils.round(predictedValues.map(denormalize())[0])
+    });
 }
 
 function normalize() {
